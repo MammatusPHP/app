d7ab241f58851d99a625c529d219c9ce9b39dc22
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-11-04 02:31:47 +0100

    Use metrics package for wiring up metrics

--- etc/di/metrics.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-use WyriHaximus\Metrics\InMemory\Registry as InMemoryRegistry;
-use WyriHaximus\Metrics\Registry;
-
-return [
-    Registry::class => new InMemoryRegistry(),
-];
