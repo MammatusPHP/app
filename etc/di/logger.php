21936cf2d7522606839e7f91ad7876445d359018
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-04-08 21:06:50 +0200

    Run logger in it's own thread

--- etc/di/logger.php
+++ etc/di/logger.php
@@ -1,23 +1,29 @@
-<?php declare(strict_types=1);
+<?php
 
+declare(strict_types=1);
+
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
-use React\EventLoop\LoopInterface;
+use ReactParallel\ObjectProxy\Proxy;
 use WyriHaximus\Monolog\Factory;
-use WyriHaximus\React\PSR3\Stdio\StdioLogger;
+
 use function DI\env;
 use function DI\factory;
-use const WyriHaximus\Constants\Boolean\TRUE_;
 
 return (static fn (): array => [
-    LoggerInterface::class => factory(static function (Logger $logger): LoggerInterface {
-        return $logger;
-    }),
-    Logger::class => factory(static function (
-        LoopInterface $loop,
+    LoggerInterface::class => factory(static function (
+        Proxy $proxy,
         string $version
-    ): Logger {
-        return Factory::create('', StdioLogger::create($loop)->withHideLevel(TRUE_), ['version' => $version]);
+    ): LoggerInterface {
+        return $proxy->thread(
+            Factory::create(
+                '',
+                new Logger('', [new StreamHandler('php://stdout')]),
+                ['version' => $version]
+            ),
+            LoggerInterface::class
+        );
     })->
         parameter('version', env('APP_VERSION', 'dev-' . time())),
 ])();
