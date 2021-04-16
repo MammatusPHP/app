7acca21aca9b6d61e6b0f2cacb442f88519ea51a
Cees-Jan Kiewiet <git@wyrihaximus.net>
2021-04-16 11:35:42 +0200

    Add proxy list interface to DI

--- etc/di/object-proxy.php
+++ etc/di/object-proxy.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 use ReactParallel\Factory;
 use ReactParallel\ObjectProxy\Configuration;
+use ReactParallel\ObjectProxy\Generated\ProxyList;
 use ReactParallel\ObjectProxy\Proxy;
+use ReactParallel\ObjectProxy\ProxyListInterface;
 use WyriHaximus\Metrics\Registry;
 
 return [
+    ProxyListInterface::class => static fn(ProxyList $proxyList): ProxyListInterface => $proxyList,
     Proxy::class => static function (Factory $factory, Registry $registry) {
         $proxy = (new Proxy((new Configuration($factory))->withMetrics(Configuration\Metrics::create($registry))));
         $proxy->create($registry, Registry::class, true);
